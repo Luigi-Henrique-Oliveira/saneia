07a32b0f84d07b8b615ce34cd152ce441c9dd586
Sergio <sergiosilvasousa524@gmail.com>
2025-09-02 18:35:25 -0300

    Base do projeto

---
+++ app/src/main/java/com/sergio/saneia/navigation/Screen.kt
@@ -0,0 +1,9 @@
+package com.sergio.saneia.navigation
+
+sealed class Screen(val route:String) {
+    object HomeScreen: Screen("home")
+    object Second Screen: Screen("home")
+    object HomeScreen: Screen("home")
+    object HomeScreen: Screen("home")
+
+}

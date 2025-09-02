e56043f55fd497cbef4fa47f1bf5b415c707c576
Sergio <sergiosilvasousa524@gmail.com>
2025-09-02 18:45:57 -0300

    Consertei o Screen

--- app/src/main/java/com/sergio/saneia/navigation/Screen.kt
+++ app/src/main/java/com/sergio/saneia/navigation/Screen.kt
@@ -2,8 +2,8 @@ package com.sergio.saneia.navigation
 
 sealed class Screen(val route:String) {
     object HomeScreen: Screen("home")
-    object Second Screen: Screen("home")
-    object HomeScreen: Screen("home")
-    object HomeScreen: Screen("home")
+    object SecondScreen: Screen("second")
+    object ThirdScreen: Screen("third")
+    object FourScreen: Screen("four")
 
 }

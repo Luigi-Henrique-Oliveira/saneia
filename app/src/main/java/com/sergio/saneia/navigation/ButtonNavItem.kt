b3ca5b9449a2d87de4582991597ff64e00bc0154
Sergio <sergiosilvasousa524@gmail.com>
2025-09-02 21:05:18 -0300

    Modelo padrao de butao

---
+++ app/src/main/java/com/sergio/saneia/navigation/ButtonNavItem.kt
@@ -0,0 +1,9 @@
+package com.sergio.saneia.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class ButtonNavItem (
+    val route: String,
+    val icon: ImageVector,
+    val label: String
+)

2539613e82fc1edeb154297c6f2ccf97e19b32ba
Sergio <sergiosilvasousa524@gmail.com>
2025-09-03 21:11:27 -0300

    Modifiquei para o novo padrao

--- app/src/main/java/com/sergio/saneia/navigation/ButtonNavItem.kt
+++ app/src/main/java/com/sergio/saneia/navigation/ButtonNavItem.kt
@@ -1,9 +1,11 @@
 package com.sergio.saneia.navigation
 
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class ButtonNavItem (
     val route: String,
-    val icon: ImageVector,
-    val label: String
+    val icon: ImageVector?=null,
+    val label: String,
+    val painter: Painter?=null
 )

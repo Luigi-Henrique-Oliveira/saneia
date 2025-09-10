246ad3a35df3d7497b8ca0538fe2784b374a775d
Sergio <sergiosilvasousa524@gmail.com>
2025-09-09 21:49:49 -0300

    Mapa sem funcionar

--- app/src/main/java/com/sergio/saneia/screens/SecundScreen.kt
+++ app/src/main/java/com/sergio/saneia/screens/SecundScreen.kt
@@ -1,9 +1,52 @@
 package com.sergio.saneia.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
+import org.osmdroid.tileprovider.MapTile
+import org.osmdroid.tileprovider.tilesource.ITileSource
 
 @Composable
-fun SecondScreen(){
-    Text(text = "Opa")
+fun SecondScreen() {
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            MapView(context).apply {
+                setMultiTouchControls(true)
+
+                // → Troca para satélite (Esri World Imagery)
+                val esriSat = object : OnlineTileSourceBase(
+                    "EsriWorldImagery",
+                    0, 22, 256, ".jpg",
+                    arrayOf("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/")
+                ) {
+                    override fun getTileURLString(pMapTileIndex: Long): String {
+                        return baseUrl + getZoom(pMapTileIndex) + "/" +
+                                getY(pMapTileIndex) + "/" +
+                                getX(pMapTileIndex) + mImageFilenameEnding
+                    }
+                }
+                setTileSource(esriSat as ITileSource)
+
+                // Coordenadas da Rua Vicente Oliveira de Brito (Juazeiro do Norte - CE)
+                val latitude = -7.2381185
+                val longitude = -39.3065841
+
+                controller.setZoom(18.0)
+                controller.setCenter(GeoPoint(latitude, longitude))
+
+                // Ponteiro padrão
+                val marker = Marker(this)
+                marker.position = GeoPoint(latitude, longitude)
+                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                marker.title = "Rua Vicente Oliveira de Brito"
+                overlays.add(marker)
+            }
+        }
+    )
 }

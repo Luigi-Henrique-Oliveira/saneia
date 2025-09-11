d2e73223de7fbb768050e921a31f91994af3c3e4
Luigi Henrique Oliveira Leite <luigih664@gmail.com>
2025-09-10 21:32:13 -0300

    ...

--- app/src/main/java/com/sergio/saneia/screens/FourScreen.kt
+++ app/src/main/java/com/sergio/saneia/screens/FourScreen.kt
@@ -1,9 +1,26 @@
 package com.sergio.saneia.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sergio.saneia.navigation.Screen
 
 @Composable
 fun FourScreen(){
-    Text(text = "Sergio 4")
+    Column (modifier = Modifier.fillMaxSize()){
+        Box {
+
+        }
+    }
+    
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FourScreenPreview(){
+    FourScreen()
 }
